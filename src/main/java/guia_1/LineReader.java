bf714cf7e15bfed5ba78d3bf64c5cb99de0cd643
Kevin <126906321+Kevingg06@users.noreply.github.com>
2025-05-09 13:07:49 -0300

    Update LineReader.java

--- src/main/java/guia_1/LineReader.java
+++ src/main/java/guia_1/LineReader.java
@@ -7,8 +7,8 @@ public class LineReader {
 	public static String leerLinea() throws IOException {
 	    StringBuilder resultado = new StringBuilder();
 	    int caracter = System.in.read();
-	    
-	    while (caracter != 13 && caracter != 10) {
+	    /*este funciona para sistemas windows, sino cambiar a caracter != 10 en sistemas unix-like*/
+	    while (caracter != 13) {
 	        resultado.append((char) caracter); 
 	        caracter = System.in.read();
 	    }
